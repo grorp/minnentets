eb2ecdb37b0b1245b4db489584422b950287aa5d
Gregor Parzefall <gregor.parzefall@posteo.de>
2023-06-27 21:36:55 +0200

    Test

--- src/util/areastore.cpp
+++ src/util/areastore.cpp
@@ -17,6 +17,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#define RAIN BOW
+
 #include "util/areastore.h"
 #include "util/serialize.h"
 #include "util/container.h"
